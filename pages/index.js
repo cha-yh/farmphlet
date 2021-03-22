98241bcc3373fc751c117e968a2ba8efccf71a9e
cha-yh <chayh311@gmail.com>
2021-03-22 16:53:35 +0900

    Initiate the next.js boilerplate

---
+++ pages/index.js
@@ -0,0 +1,23 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Farmphlet</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+         Hello world
+        </h1>
+      </main>
+
+      <footer className={styles.footer}>
+        <p>footer</p>
+      </footer>
+    </div>
+  )
+}
