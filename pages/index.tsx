1eb9b182175aebf80e1afac52e8ec7164113784f
cha-yh <chayh311@gmail.com>
2021-03-23 23:33:58 +0900

    feat: Update Home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,13 @@
 import Head from 'next/head'
+import { useDebugValue, useState } from 'react'
 import styles from '../styles/Home.module.css'
-
+function useStateWithLabel(initialValue, name) {
+  const [value, setValue] = useState(initialValue);
+  useDebugValue(`${name}: ${value}`);
+  return [value, setValue];
+}
 export default function Home() {
+  const [hello, setHello] = useStateWithLabel("world", "hello")
   return (
     <div className={styles.container}>
       <Head>
@@ -11,13 +17,9 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-         Hello world
+          Hello {hello}
         </h1>
       </main>
-
-      <footer className={styles.footer}>
-        <p>footer</p>
-      </footer>
     </div>
   )
 }
