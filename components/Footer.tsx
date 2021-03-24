21134ca99c4b2ae884510ec04b36cf497ed4d566
cha-yh <chayh311@gmail.com>
2021-03-24 22:37:46 +0900

    feat: Add footer contents & styles

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,14 +2,36 @@ import styled from "styled-components";
 
 const Footer = () => {
     return (
-        <FooterBlock></FooterBlock>
+        <FooterBlock>
+            <h1>Farmphlet</h1>
+            <p className="contact">Contact: email@gmail.com</p>
+            <p className="copyright">© Copyright - Farmphlet 2021</p>
+        </FooterBlock>
     )
 }
 
 const FooterBlock = styled.footer`
     width: 100%;
-    height: 14rem;
-    background: #012E00;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background: #06443B;
+    color: white;
+    padding: 50px;
+
+    >h1 {
+        
+    }
+
+    >.contact {
+        font-size: 14px;
+        margin-top: 20px;
+        margin-bottom: 50px;
+    }
+
+    >.copyright {
+        font-size: 14px;
+    }
 `;
 
 export default Footer;
