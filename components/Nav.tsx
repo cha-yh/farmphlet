b610db31277584ed24eda048478ab6e221f2dbca
cha-yh <chayh311@gmail.com>
2021-03-24 22:33:19 +0900

    style: Update Nav style

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -2,22 +2,34 @@ import styled from "styled-components";
 
 const Nav = () => {
     return (
-        <NavBlock>
-            <p>LOGO</p>
+        <Container>
+            <NavBlock>
+                <p>LOGO</p>
 
-            <span className="material-icons">menu</span>
+                <span className="material-icons">menu</span>
 
-        </NavBlock>
+            </NavBlock>
+        </Container>
     )
 }
 
-const NavBlock = styled.nav`
+
+const Container = styled.div`
     width: 100%;
     height: 4rem;
-    background: #012E00;
+    display: flex;
+    justify-content: center;
+    background: #07574c;
     position: fixed;
     top: 0;
-    padding: 1rem;
+    box-shadow: 0 20px 25px -5px rgba(0,0,0,0.1),0 10px 10px -5px rgba(0,0,0,0.04);
+`;
+
+const NavBlock = styled.nav`
+    max-width: 920px;
+    width: 90%;
+    height: 4rem;
+    padding: 1rem 0;
     color: white;
     display: flex;
     align-items: center;
